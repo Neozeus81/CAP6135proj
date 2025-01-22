47e4ee16dbda1686fe0945e8e04cc5d767d79418
Alex <alex.j.flohr@gmail.com>
2025-01-22 14:41:01 -0500

    dataset import cleaned

--- data/data.py
+++ data/data.py
@@ -1,6 +1,23 @@
-import kagglehub
+# this downloads the datasets into your computer
+
+"""import kagglehub
 
 # Download latest version
 path = kagglehub.dataset_download("hassan06/nslkdd")
 
 print("Path to dataset files:", path)
+"""
+import os
+from dotenv import load_dotenv
+import pandas as pd 
+
+load_dotenv()
+
+KDD_TRAIN = os.getenv("KDD_TRAIN")
+
+df = pd.read_csv(KDD_TRAIN, sep=',')
+
+print(df)
+
+
+
